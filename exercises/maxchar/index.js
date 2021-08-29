dd07bb3a78e487315b23f84498d44816037a963c
seyfer <seyferseed@gmail.com>
2021-08-29 14:56:51 +0200

    ++

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,43 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const chars = {};
+
+    for (const char of str) {
+        chars[char] = chars[char] ? ++chars[char] : 1;
+    }
+
+    const max = Math.max(...Object.values(chars));
+
+    return Object.keys(chars)
+        .reduce(function (obj, key) {
+            obj[chars[key]] = key;
+            return obj;
+        }, {})[max];
+}
 
 module.exports = maxChar;
+
+// function maxChar(str) {
+//     const charMap = {};
+//     let max = 0;
+//     let maxChar = '';
+//
+//     for (let char of str) {
+//         if (charMap[char]) {
+//             charMap[char]++;
+//         } else {
+//             charMap[char] = 1;
+//         }
+//     }
+//
+//     for (let char in charMap) {
+//         if (charMap[char] > max) {
+//             max = charMap[char];
+//             maxChar = char;
+//         }
+//     }
+//
+//     return maxChar;
+// }
